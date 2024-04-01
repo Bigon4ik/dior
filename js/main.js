e0e0595bb326c061baf22c0d59c3ae8bb836b02c
Bigon4ik <bigon444ik@gmail.com>
2024-04-01 14:12:55 +0300

    create new slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const swiper = new Swiper('.swiper', {
+const swiperTop = new Swiper('.top__swiper', {
     effect: 'fade',
     autoplay: {
         delay: 3500,
         disableOnInteraction: false,
     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const swiperAbout = new Swiper('.about__swiper', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode:true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
